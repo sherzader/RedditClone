01f5bb4faceaf9b59d3b6f72ea6545f39da0c254
Sheri <sheri.h.zada@gmail.com>
2015-11-20 14:17:01 -0800

    Finish Authentication

--- RedditClone/app/controllers/application_controller.rb
+++ RedditClone/app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    return nil if session[:session_token].nil?
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login!
+    @current_user = @user
+    session[:session_token] = @current_user.session_token
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
