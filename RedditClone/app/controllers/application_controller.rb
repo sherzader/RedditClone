f79eb74b59d9d403110b6184588de717d4d19cb6
Sheri <sheri.h.zada@gmail.com>
2015-11-20 16:02:19 -0800

    Finish creating Post Model, Controller, and View

--- RedditClone/app/controllers/application_controller.rb
+++ RedditClone/app/controllers/application_controller.rb
@@ -6,8 +6,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    return nil if session[:session_token].nil?
-    @current_user ||= User.find_by(session_token: session[:session_token])
+    # return nil if session[:session_token].nil?
+    if session[:session_token]
+      @current_user ||= User.find_by(session_token: session[:session_token])
+    end
   end
 
   def login!(user)
--- RedditClone/config/routes.rb
+++ RedditClone/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
   resources :subs, except: [:destroy]
+  resources :posts, except: [:index]
 end
